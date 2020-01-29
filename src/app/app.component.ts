cfc6b5d5356f39d1e518784ba8757151c6908b4f
IanJPeek <IanJPeek@gmail.com>
2020-01-29 11:41:08 +0000

    tutorial scrapped/ paused

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, Renderer2 } from '@angular/core';
+
+//tutorial from https://www.dev6.com/angular/capturing-camera-images-with-angular/
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+@ViewChild('video', {static:true}) videoElement: ElementRef;
+  @ViewChild('canvas', { static: true }) canvas: ElementRef;
+
   title = 'capture';
 }
+
+
+// May need to be in CSS?
+
+// constructor(private renderer: Renderer2) { }
